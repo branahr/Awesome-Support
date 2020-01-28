cded9dc9129836f32c0b72979ac6c9254689fada
Branahr <brana.hr@gmail.com>
2020-01-28 18:01:44 +0100

    Sanitized post_status_override variable

--- includes/admin/functions-post.php
+++ includes/admin/functions-post.php
@@ -92,10 +92,10 @@ function wpas_filter_ticket_data( $data, $postarr ) {
 
 		if ( array_key_exists( $_POST['post_status_override'], $status ) ) {
 
-			$data['post_status'] = $_POST['post_status_override'];
+			$data['post_status'] = sanitize_key($_POST['post_status_override']);
 
 			if ( isset($postarr['original_post_status']) && $postarr['original_post_status'] !== $_POST['post_status_override'] && isset( $_POST['wpas_post_parent'] ) ) {
-				wpas_log_history( intval( $_POST['wpas_post_parent'] ), sprintf( __( 'Ticket state changed to %s', 'awesome-support' ), '&laquo;' . $status[ $_POST['post_status_override'] ] . '&raquo;' ) );
+				wpas_log_history( intval( $_POST['wpas_post_parent'] ), sprintf( __( 'Ticket state changed to %s', 'awesome-support' ), '&laquo;' . $status[ sanitize_key( $_POST['post_status_override'] ) ]  . '&raquo;' ) );
 			}
 		}
 
